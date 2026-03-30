1d7aabf2f61bbc6e4344bf6579181091d7e8f7bd
Ryan R <33332787+RyannR9375@users.noreply.github.com>
2026-03-29 19:22:18 -0700

    Upgrades Per Tower Change

--- Assets/Scripts/TowerUpgradePanel.cs
+++ Assets/Scripts/TowerUpgradePanel.cs
@@ -1,29 +1,53 @@
 using TMPro;
 using UnityEngine;
 
-public class StoreUI : MonoBehaviour
+public class TowerUpgradePanel : MonoBehaviour
 {
+    public static TowerUpgradePanel instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    
     [Header("Panels")]
     public GameObject storePanel;
 
     [Header("UI Text")]
-    public TMP_Text currencyText;
-
     public TMP_Text damageCostText;
     public TMP_Text fireRateCostText;
     
     [Header("Upgrade Costs")]
     public int damageUpgradeCost = 50;
     public int fireRateUpgradeCost = 50;
+
+    [Header("Upgrade Stats")] 
+    public int damageIncrease = 2;
+    public int fireRateIncrease = 2;
     
+    [Header("Upgrade UI Offset")]
+    public float xOffset;
+    public float yOffset;
+
+    private Tower _selectedTower;
 
     public void OpenStore()
     {
-        if (storePanel != null)
-            storePanel.SetActive(true);
+        if (!storePanel) return;
 
+        storePanel.SetActive(true);
+        storePanel.transform.position = OffsetPos();
+        
         RefreshUI();
         Time.timeScale = 0f;
+    }
+    
+    private Vector3 OffsetPos()
+    {
+        return new Vector3(
+            Camera.main.WorldToScreenPoint(_selectedTower.transform.position).x + xOffset, 
+            Camera.main.WorldToScreenPoint(_selectedTower.transform.position).y + yOffset, 
+            0);
     }
 
     public void CloseStore()
@@ -41,11 +65,13 @@ public class StoreUI : MonoBehaviour
 
         GameManager.Instance.SpendCurrency(damageUpgradeCost);
 
-        Tower[] towers = FindObjectsOfType<Tower>();
+        /*Tower[] towers = FindObjectsOfType<Tower>();
         foreach (Tower tower in towers)
         {
             tower.damage += 2;
-        }
+        }*/
+        if (!_selectedTower) return;
+        _selectedTower.damage += damageIncrease;
 
         RefreshUI();
     }
@@ -57,25 +83,29 @@ public class StoreUI : MonoBehaviour
 
         GameManager.Instance.SpendCurrency(fireRateUpgradeCost);
 
-        Tower[] towers = FindObjectsOfType<Tower>();
+        /*Tower[] towers = FindObjectsOfType<Tower>();
         foreach (Tower tower in towers)
         {
             tower.fireRate += 0.5f;
-        }
+        }*/
+        
+        if(!_selectedTower) return;
+        _selectedTower.fireRate += fireRateIncrease;
 
         RefreshUI();
     }
 
     public void RefreshUI()
     {
-        if (currencyText != null && GameManager.Instance != null)
-        {
-            currencyText.text = "Currency: " + GameManager.Instance.currency;
-        }
-        damageCostText.text = "Damage Cost: " + damageUpgradeCost;
-        fireRateCostText.text = "Fire Rate Cost: " + fireRateUpgradeCost;
+        damageCostText.text = "Cost: " + damageUpgradeCost;
+        fireRateCostText.text = "Cost: " + fireRateUpgradeCost;
     }
     
     public int GetFireRateUpgradeCost(){ return fireRateUpgradeCost; }
     public int GetDamageUpgradeCost(){ return damageUpgradeCost; }
+
+    public void SetSelectedTower(Tower newTower)
+    {
+        _selectedTower = newTower;
+    }
 }
